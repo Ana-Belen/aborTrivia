968f3629351621b03761c223808a95d90e2320d5
Ana Barrionuevo <abarrionuevo@modusagency.com>
2018-06-25 19:40:00 -0300

    half way done button actions

--- game-aborto/src/components/optionButton.js
+++ game-aborto/src/components/optionButton.js
@@ -8,15 +8,27 @@ class OptionButton extends React.Component {
     constructor(props){
         super(props);
         this.calcScore = this.calcScore.bind(this);
+        this.defineClassNames = this.defineClassNames.bind(this);
     }
     calcScore(id){
         window.props = this.props;
         if(id === this.props.rightOne){
-            this.props.actions.increaseScore(10);
+            this.defineClassNames();
+            setTimeout(
+                this.props.actions.increaseScore(10)
+            , 2000);
         }else{
+            this.defineClassNames();
             this.props.actions.wrongScore(1);
         }
 
+    }
+
+    defineClassNames(){
+        this.props.feedback = [];
+        for (let i = 0; this.props.options.length ; i++){
+            this.props.feedback[i] = (this.props.options[i].id === this.props.rightOne)? 'right' : 'wrong';
+        }
     }
 
     render(){
@@ -25,7 +37,8 @@ class OptionButton extends React.Component {
              console.log('---> ITEM:', item);
             return(
                 <button
-                key={index}
+                className={this.props.feedback && this.props.feedback[index]}
+                key={item.id}
                 onClick={()=>{this.calcScore(item.id)}}
                 >{`Dip. ${item.apellido}`}</button>
             ) 
